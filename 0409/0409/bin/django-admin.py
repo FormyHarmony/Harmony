921eb985e4a424eb13dde17c52b0b5d25d3cb621
FormyHarmony <36830367+FormyHarmony@users.noreply.github.com>
2018-04-11 13:46:53 +0800

    0409

---
+++ 0409/0409/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/harmony/testvirtual/testvirtual/env1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ 0409/0409/mysite/howhow/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HowhowConfig(AppConfig):
+    name = 'howhow'
