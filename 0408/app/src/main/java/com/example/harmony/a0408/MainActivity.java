aa653adecddbc20f7b4b7b2e6a9d9b9e7b794bf0
FormyHarmony <36830367+FormyHarmony@users.noreply.github.com>
2018-04-30 10:16:06 +0800

    ~

--- 0408/app/src/main/java/com/example/harmony/a0408/MainActivity.java
+++ 0408/app/src/main/java/com/example/harmony/a0408/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.view.View;
+import android.view.View;//OnClickListener 使用 View 類型變數
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btndo;
@@ -15,16 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//取得介面資訊
         btndo=(Button)findViewById(R.id.button);
         inputext=(EditText)findViewById(R.id.edit);
         text=(TextView)findViewById(R.id.textword);
-        btndo.setOnClickListener(listener);
+        btndo.setOnClickListener(listener);//設定按鈕的觸發事件；btndo 按鈕名稱
     }
     private Button.OnClickListener listener=new Button.OnClickListener(){
         @Override
         public void onClick(View v ){
-        input=inputext.getText().toString();
+        input=inputext.getText().toString();//android text editable
         text.setText("你的學號是"+input);
         }
     };
